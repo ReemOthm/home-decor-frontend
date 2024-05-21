eba228ae9bc0d14d0fc558c75c073b8e8329e558
ReemOthm <ReemOth@outlook.com>
2024-05-21 18:35:27 +0300

    done filter products by category

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,24 +1,33 @@
-import { Box, Button, Grid, Pagination, Stack } from "@mui/material";
+import { Autocomplete, Box, Button, Grid, Pagination, Stack, TextField } from "@mui/material";
 import { Helmet } from "react-helmet";
 import { useRef, useState } from "react";
 
 import useApiQuery from "./../hooks/useApiQuery";
 import ProductCard from "@/components/ui/ProductCard";
-import { Product } from "@/types";
+import { Category, Product } from "@/types";
 
 export const Products = ()=>{
 
     const [pageNumber, setPageNumber] = useState(1);
     const [searchKeyword, setSearchKeyword] = useState<string>("");
+    const [category, setCategory] = useState<string>("");
 
     const keyword = useRef<HTMLInputElement>(null);
 
     // Queries
     const { data, error, isLoading } = useApiQuery({
-        queryKey: ["products", `${pageNumber}`, `${searchKeyword}`],
+        queryKey: ["products", `${pageNumber}`, `${searchKeyword}`, `${category}`],
         method: "get" ,
-        url: `/products?keyword=${searchKeyword}&page=${pageNumber}`
+        url: `/products?keyword=${searchKeyword}&category=${category}&page=${pageNumber}`
     });
+
+    const { data: categories} = useApiQuery({
+        queryKey: ["categories"],
+        method: "get" ,
+        url: `/categories`
+    });
+
+    const categoryOptions = categories != null ? [{name: "All"}, ...categories.data] : null
 
     const handlePageNumber = (event: React.ChangeEvent<unknown>,page: number)=> setPageNumber(page)
 
@@ -35,6 +44,17 @@ export const Products = ()=>{
         setPageNumber(1);
     }
 
+    const handleFilterByCategory = (event: React.SyntheticEvent<Element, Event>, value: string | null)=> {
+        setPageNumber(1)
+        
+        if(value != null)
+            setCategory(value)
+
+        if(value == "All")
+            setCategory("")
+    }
+
+
     if(isLoading) return <h1>Products are loading</h1>
 
     return (
@@ -43,10 +63,30 @@ export const Products = ()=>{
             {
                 data && 
                 <>
-                    <form className="search" onSubmit={handleSearch}>
-                        <input type="search" ref={keyword}/>
-                        <input type="submit" value="search" />
-                    </form>
+                    <Stack direction="row" justifyContent="space-between">
+                        <form className="search" onSubmit={handleSearch}>
+                            <input type="search" ref={keyword}/>
+                            <input type="submit" value="search" />
+                        </form>
+
+                    { categoryOptions && categoryOptions.length > 0 &&
+                        <Autocomplete 
+                            sx={{width: "200px"}}
+                            onChange={handleFilterByCategory}
+                            size= "small"
+                            options={categoryOptions.map((category) => category.name)}
+                            renderInput={(params) => (
+                            <TextField 
+                                {...params}
+                                label="Filter by Category"
+                                InputProps={{
+                                ...params.InputProps,
+                                }}
+                            />
+                            )}
+                        />
+                    }    
+                    </Stack>
 
                     { data.items.length > 0 ?
                         <>
